0a2c69b212ded6681162f13883017123cd574142
murilo <murilocarrijoadm@gmail.com>
2022-05-25 16:27:00 -0300

    feat: Criando a subclasse  Warrior.ts

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,23 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  private static _countWarrior = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+  }
+
+  static createdArchetypeInstances(): number {
+    this._countWarrior += 1;
+    return this._countWarrior;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Warrior;
