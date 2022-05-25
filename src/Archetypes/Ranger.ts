03d1c5ccb0e95208ec88c6995246c3bb3348408c
murilo <murilocarrijoadm@gmail.com>
2022-05-25 16:28:53 -0300

    feat: Criando a subclasse  Ranger.ts

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,23 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  private static _countRanger = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+  }
+
+  static createdArchetypeInstances(): number {
+    this._countRanger += 1;
+    return this._countRanger;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Ranger;
